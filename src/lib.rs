68bb421ce658680952c19311dfef9921ae260b00
Paul Loyd <pavelko95@gmail.com>
2022-01-05 22:56:13 +0300

    feat: support `const`, `static` and `type`

--- src/lib.rs
+++ src/lib.rs
@@ -20,7 +20,7 @@
 //! does and how to use it.
 
 use proc_macro::TokenStream;
-use syn::{Item, parse_macro_input};
+use syn::{parse_macro_input, Item};
 
 mod unstable;
 
@@ -103,11 +103,14 @@ pub fn unstable(args: TokenStream, item: TokenStream) -> TokenStream {
     let attr = unstable::UnstableAttribute::from(args);
 
     match parse_macro_input!(item as Item) {
+        Item::Type(item_type) => attr.expand(item_type),
         Item::Enum(item_enum) => attr.expand(item_enum),
+        Item::Struct(item_struct) => attr.expand(item_struct),
         Item::Fn(item_fn) => attr.expand(item_fn),
         Item::Mod(item_mod) => attr.expand(item_mod),
-        Item::Struct(item_struct) => attr.expand(item_struct),
         Item::Trait(item_trait) => attr.expand(item_trait),
+        Item::Const(item_const) => attr.expand(item_const),
+        Item::Static(item_static) => attr.expand(item_static),
         _ => panic!("unsupported item type"),
     }
 }
